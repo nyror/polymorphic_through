5c5d2d8c7f4447b09c23c4cba9c721a7fdebf4a1
Ken Lee <ken@seeice.com>
2013-10-29 15:06:31 -0400

    poymorephic relation works well

--- app/models/billable_week.rb
+++ app/models/billable_week.rb
@@ -1,2 +1,3 @@
 class BillableWeek < ActiveRecord::Base
+  has_many :comments, as: :commentable
 end
--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -1,2 +1,3 @@
 class Timesheet < ActiveRecord::Base
+  has_many :comments, as: :commentable
 end
